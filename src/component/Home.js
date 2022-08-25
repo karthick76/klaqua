64e5041742ba567dd3ef92ddc72836b12b1eeb8d
Karthick Raja Suresh (INFOSYS LIMITED) <v-kasuresh@microsoft.com>
2022-08-25 20:16:00 +0530

    first commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
-import { maxHeight } from '@mui/system';
 export default class Home extends Component {
   render() {
     return (
